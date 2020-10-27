11ecb1729ce2e2352beac496d425e05e59b82254
ssruoh <ssruoh@gmail.com>
2020-10-27 00:14:48 +0000

    Functioning emailjs set up for Contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("service_8tyxpul", "La Petite Sirene", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "event_enquiry": contactForm.eventenquiry.value
+    })
+    .then(
+        function(response) {
+            console.log("success", response);
+        },
+        function(error) {
+            console.log("failure", error);
+        });
+        return false;
+}
